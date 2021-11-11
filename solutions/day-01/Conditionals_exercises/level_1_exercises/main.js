84a91f0ecf975654c0b078ce67f3dc0a987c55d9
Houghjon <jonathan_gauguin@hotmail.dk>
2021-11-11 17:41:19 +0100

    Conditionals exercises added

---
+++ solutions/day-01/Conditionals_exercises/level_1_exercises/main.js
@@ -0,0 +1,40 @@
+//#1
+/*let age = prompt('Enter your age: ')
+switch (true){
+    case age < 18 :
+        console.log ('You need to wait ' + Math.ceil(18-age) + ' years to drive.')
+        break
+
+    case age > 17 :
+        console.log('You are old enough to drive.')
+        break
+    
+}*/
+//#2
+/*let myAge = 25
+let yourAge = prompt('Enter your age: ')
+let ageDiff = Math.abs(yourAge-myAge)
+if (yourAge > myAge) {
+    console.log('Hey lad, you are ' + ageDiff + " years older than me!")
+    
+} else {
+    console.log("I'm " +ageDiff+ " years older than you")
+    
+}*/
+//#3
+// first using if else statement
+/*
+let a = prompt('Enter value of a: ')
+let b = 3
+
+if (a >b ) {
+    console.log('A is greater than B.')
+    
+} else {
+    console.log('A is not greater than B.')
+    
+}
+
+//using ternary statement
+ a > b ? console.log('A is greater than B.') : console.log('A is not greater than B.')
+ */
